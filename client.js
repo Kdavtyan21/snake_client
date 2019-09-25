f0495b81a996dd4c9765fa97e28ce28e36f827ab
Karen <karendavtyan21@mail.com>
2019-09-25 18:13:35 +0000

    moving snake

--- client.js
+++ client.js
@@ -7,17 +7,19 @@ const connect = function() {
     port: 50541
   });
   conn.on('data', (data) => {
-  console.log(data);
-  });
-
-  conn.on('connect', () => {
-    console.log('Successfully connected to game server')
+    console.log(data);
+    });
+    conn.on('connect', () => {
     conn.write("Name: KEK")
-  });
-
-
-
-
+      console.log('Successfully connected to game server')
+    //   setTimeout(() => {
+    //   conn.write("Move: up")
+    // }, 2000)
+    });
+  // "Move: up" - move up one square (unless facing down)
+  // "Move: down" - move down one square (unless facing up)
+  // "Move: left" - move left one square (unless facing right)
+  // "Move: right" - move left one square (unless facing left)
 
   conn.setEncoding('utf8'); 
   return conn;
