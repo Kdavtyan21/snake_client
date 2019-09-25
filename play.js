1dbd5fcb643e4116d65e09394077ee9a7792c710
Karen <karendavtyan21@mail.com>
2019-09-25 18:28:56 +0000

    adding exit

--- play.js
+++ play.js
@@ -1,3 +1,15 @@
 const connect= require('./client')
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  return stdin;
+}
+const handleUserInput = function(input){
+ if (input === '\u0003')
+  process.exit();
+};
 console.log('Connecting ...');
 connect();
