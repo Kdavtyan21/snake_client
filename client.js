3181551abd1a295a342a0fdc749b2a9f7a268f19
Karen <karendavtyan21@mail.com>
2019-09-25 19:16:25 +0000

    moving with keys

--- client.js
+++ client.js
@@ -12,15 +12,9 @@ const connect = function() {
     conn.on('connect', () => {
     conn.write("Name: KEK")
       console.log('Successfully connected to game server')
-    //   setTimeout(() => {
-    //   conn.write("Move: up")
-    // }, 2000)
     });
-  // "Move: up" - move up one square (unless facing down)
-  // "Move: down" - move down one square (unless facing up)
-  // "Move: left" - move left one square (unless facing right)
-  // "Move: right" - move left one square (unless facing left)
 
+    
   conn.setEncoding('utf8'); 
   return conn;
 }
--- input.js
+++ input.js
@@ -1,4 +1,7 @@
-const setupInput = function() {
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -9,8 +12,17 @@ const setupInput = function() {
   return stdin;
 }
 const handleUserInput = function(input){
-  if (input === '\u0003')
-  process.exit();
+  if (input === '\u0003') {
+    process.exit();
+  } else if (input === 'w') {
+    connection.write("Move: up")
+  } else if (input === 'd') {
+    connection.write("Move: right")
+  } else if (input === 's') {
+    connection.write("Move: down")
+  } else if (input === 'a') {
+    connection.write("Move: left")
+  }
 };
 
 module.exports = {setupInput}
